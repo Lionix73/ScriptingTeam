4728d8bf7b9c4633ba859f92a244eada86346553
Alejandro Lopez <alejolr1218@gmail.com>
2024-01-28 15:59:43 -0500

    Todos los ejercicios

---
+++ Ejercicio 18 (funciones)/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//Ejercicio 18
+
+using System;
+
+Console.Write("Ingresa un número de máximo 8 cifras: ");
+int numero = int.Parse(Console.ReadLine());
+int resultado = SumaDeNumeros(numero);
+Console.WriteLine($"La suma de los dígitos hasta obtener un solo número es: {resultado}");
+
+int SumaDeNumeros(int num)
+{
+    while (num > 9)
+    {
+        int suma = 0;
+
+        while (num > 0)
+        {
+            suma += num % 10;
+            num /= 10;
+        }
+
+        num = suma;
+    }
+
+    return num;
+}
+
---
+++ Ejercicio 6 (ciclo do while)/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int suma = 0;
+int numero = 1;
+
+Console.Write("Ingrese el valor de n: ");
+int n = int.Parse(Console.ReadLine());
+
+do
+{
+    suma += numero;
+    numero += 2; 
+    n--;
+} while (n > 0);
+
+Console.WriteLine($"La suma de los primeros {n} números impares es: {suma}");
